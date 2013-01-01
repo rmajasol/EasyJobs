66753e56c53ade23dc34a8eda29fe0b72d524960
Ramon Majasol <tishobueno@gmail.com>
2013-01-01 22:58:02 +0100

    Trying to fix heroku screen.css isn't precompiled):

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Easyjobs
 
     # If you are deploying Rails 3.1 on Heroku, to force application to not access the DB
     # or load models when precompiling your assets.
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
