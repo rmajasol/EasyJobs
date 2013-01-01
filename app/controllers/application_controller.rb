ed3d05da619ff9f556c482e95a59571b78db4283
Ramon Majasol <tishobueno@gmail.com>
2013-01-01 18:54:12 +0100

    Restricting access to post a new job only for signed in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,26 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+    protect_from_forgery
 
-  # hace que los métodos 'current_user_session' y 'current_user' sean
-  # accesibles desde las vistas
-  helper_method :current_user_session, :current_user
+    # hace que los métodos 'current_user_session' y 'current_user' sean
+    # accesibles desde las vistas
+    helper_method :current_user_session, :current_user
 
-  private
+    private
 
-  def current_user_session
-  	return @current_user_session if defined?(@current_user_session)
-  	@current_user_session = UserSession.find
-  end
+    def current_user_session
+    	return @current_user_session if defined?(@current_user_session)
+    	@current_user_session = UserSession.find
+    end
 
-  def current_user
-  	return @current_user if defined?(@current_user)
-  	@current_user = current_user_session && current_user_session.user
-  end
+    def current_user
+    	return @current_user if defined?(@current_user)
+    	@current_user = current_user_session && current_user_session.user
+    end
+
+    def require_current_user
+        unless current_user
+            flash[:error] = 'You must be logged in'
+            redirect_to root_path
+        end
+    end
 end
