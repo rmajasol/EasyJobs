97dd1fd4972190277bf54110523c7d36d7802f68
Ramon Majasol <tishobueno@gmail.com>
2013-01-01 21:54:00 +0100

    Styled some forms and generally cleaned things up

--- bash_history.sh
+++ bash_history.sh
@@ -3,3 +3,11 @@ git branch style
 
 # nos cambiamos a la nueva rama
 git checkout style
+
+git add .
+git commit -m "Styled the index view"
+
+# vamos a la rama master y la mezclamos con lo que hicimos en la style
+git checkout master
+git merge style
+git push
